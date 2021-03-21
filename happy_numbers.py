944dd65bb3f8b9db39e382bf191de530ea5f43a6
Axis <otakugaming088@gmail.com>
2021-03-21 22:33:37 +0530

    test again

--- happy_numbers.py
+++ happy_numbers.py
@@ -1,12 +1,3 @@
-'''
-Happy Numbers - A happy number is defined by the following process. Starting with any positive integer,
-replace the number by the sum of the squares of its digits, 
-and repeat the process until the number equals 1 (where it will stay), 
-or it loops endlessly in a cycle which does not include 1. 
-Those numbers for which this process ends in 1 are happy numbers, 
-while those that do not end in 1 are unhappy numbers. 
-Display an example of your output here. Find first 8 happy numbers.
-'''
 
 def happy_number(n):
     n1 = sum([int(letter) ** 2 for letter in str(n)])
@@ -48,5 +39,3 @@ def isHappyNumber(num):
 
 print(happy_number(7))
 print(isHappyNumber(1))
-
-a = 10
