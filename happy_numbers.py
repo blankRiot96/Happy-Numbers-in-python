919ab7a83ad0060fb9a1cd54dfb7ad2b437dec48
Axis <otakugaming088@gmail.com>
2021-03-21 22:30:12 +0530

    Test

--- happy_numbers.py
+++ happy_numbers.py
@@ -19,5 +19,34 @@ def happy_number(n):
 
     return happy
 
+#isHappyNumber() will determine whether a number is happy or not    
+def isHappyNumber(num):   
+    count2 = 0;  
+    while True and count2 <= 8:
+        rem = sum = 0;    
+            
+        #Calculates the sum of squares of digits    
+        while(num > 0):    
+            rem = num%10;    
+            sum = sum + (rem*rem);    
+            num = num//10;    
+        return sum;    
+
+    num += 1;  
+    result = num;    
+        
+    while(result != 1 and result != 4):    
+        result = isHappyNumber(result);    
+        
+    #Happy number always ends with 1    
+    if(result == 1):    
+        print(str(num) + " is a happy number");    
+        count2 += 1; 
+    #Unhappy number ends in a cycle of repeating numbers which contain 4    
+    elif(result == 4):    
+        print(str(num) + " is not a happy number");  
 
 print(happy_number(7))
+print(isHappyNumber(1))
+
+a = 10
